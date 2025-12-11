c00a4dd4d3149c9b1206690d4f7d73f1f2a8c8af
Matheus Mikio <matheusmikio15@gmail.com>
2025-12-10 23:57:22 -0300

    Neat type of service price

--- schemas/service.go
+++ schemas/service.go
@@ -5,7 +5,7 @@ import "github.com/shopspring/decimal"
 type Service struct {
 	Name      string          `json:"name"`
 	Duration  string          `json:"duration"`
-	Price     decimal.Decimal `json:"price"`
+	Price     decimal.Decimal `json:"price" gorm:"type:decimal(10,2)"`
 	CompanyID uint            `json:"companyId"`
 	Company   Company         `json:"-" gorm:"foreignKey:CompanyID"`
 }
