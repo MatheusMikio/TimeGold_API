3c6cada39a876900f3eef59c041c6d7127e18631
Matheus Mikio <matheus.mikio@interfocus.com.br>
2025-12-17 08:41:52 -0300

    handler createClient

---
+++ handlers/client/createClient.go
@@ -0,0 +1,5 @@
+package client
+
+func CreateClientHandler(ctz *gin.Context){
+	return nil
+}
--- router/initClient.go
+++ router/initClient.go
@@ -3,6 +3,6 @@ package router
 func initClient(rg *gin.RouterGroup){
 	client := rg.Group("/clients")
 	{
-		
+		client.POST("", CreateClientHandler)
 	}
 }
